51ac739b3911be0977d90c59972827af7291b441
EC2 Default User <ec2-user@ip-172-31-12-118.us-east-2.compute.internal>
2020-03-07 08:35:42 +0000

    set validation

--- app/models/shrine.rb
+++ app/models/shrine.rb
@@ -1,2 +1,7 @@
 class Shrine < ApplicationRecord
+    validates :name, presence: true
+    validates :adress, presence: true
+    validates :nearest_station, presence: true
+    validates :location, presence: true
+    validates :user_id, presence: true
 end
